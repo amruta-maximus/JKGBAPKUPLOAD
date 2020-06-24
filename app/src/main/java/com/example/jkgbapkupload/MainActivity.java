c651078334249544bbd7d484689bd486259bed91
yoyonitusingh <amrutagole247@gmaill.com>
2020-06-24 19:19:45 +0530

    debug apk

--- app/src/main/java/com/example/jkgbapkupload/MainActivity.java
+++ app/src/main/java/com/example/jkgbapkupload/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String downloadAPKUrl = "https://github.com/amru-nitin/GPSApplication/raw/master/app-debug.apk";
+    public static final String downloadAPKUrlq = "https://github.com/amruta-maximus/JKGBAPKUPLOAD/raw/master/JKGB_MATM_PRO_1.apk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
